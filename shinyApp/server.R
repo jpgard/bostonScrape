5a1d515e84cacf821a35651a64f62c837fbad489
Josh Gardner <joshua.patrick.gardner@gmail.com>
2016-04-27 22:47:52 -0400

    updated
    updated inputs

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -1,4 +1,7 @@
 library(shiny)
 shinyServer(
     function(input, output) {
+        #position in overall results
+        
+        #position in age group/gender
     } )
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -2,8 +2,13 @@ library(shiny)
 shinyUI(pageWithSidebar(
     headerPanel("Hello Shiny!"),
     sidebarPanel(
-        h3('Sidebar text')
+        h3('Control Panel'), 
+        p('Enter your information here to explore your results.'),
+        selectInput("gender", label = "Gender", c("Male" = "M", "Female" = "F")),
+        selectInput("agegroup", label = "Age Group", c("18-34", "35-39", "40-44", "45-49", "50-54", "60-64", "65-69", "70-74", "75-79", "80+")),
+        sliderInput("bib_number", label = "Bib Number", min = 1, max = 40000, value = 1, step = 1)
     ),
     mainPanel(
-        h3('Main Panel text')
+        h3('Boston Marathon Results Explorer')
+        p('Enter Your ')
     ) ))
