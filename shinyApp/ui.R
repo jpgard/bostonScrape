2b49493483d4566d711ce4c10f83fa08b722bfac
Josh Gardner <joshua.patrick.gardner@gmail.com>
2016-04-27 22:21:38 -0400

    initial upload
    initial shiny app

---
+++ shinyApp/ui.R
@@ -0,0 +1,9 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+    headerPanel("Hello Shiny!"),
+    sidebarPanel(
+        h3('Sidebar text')
+    ),
+    mainPanel(
+        h3('Main Panel text')
+    ) ))
